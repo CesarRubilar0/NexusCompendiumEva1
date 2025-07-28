ee30a819bd8721162a286ea631fafcbadc566198
Eduardo <eduardo.johnson.guerrero@estudiante.ipss.cl>
2025-07-28 01:38:19 -0400

    Arreglo archivo artisan

--- IPSS-2Año/Segundo semetres/Web/NexusCompendiumEva1/config/app.php
+++ IPSS-2Año/Segundo semetres/Web/NexusCompendiumEva1/config/app.php
@@ -21,7 +21,7 @@ return [
                 'driver' => 'mysql',
                 'host' => '127.0.0.1',
                 'port' => '3306',
-                'database' => 'ipss_projects',
+                'database' => 'conecta_ip',
                 'username' => 'root',
                 'password' => '',
                 'charset' => 'utf8mb4',
